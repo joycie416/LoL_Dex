46ebcd1d73d6e3f106378b6f3bdf01f501403982
joycie416 <joycie416@gmail.com>
2024-10-01 15:47:55 +0900

    [add] base files

---
+++ src/app/champions/[id]/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const ChapionDetailPage = () => {
+  return (
+    <div>ChapionDetailPage</div>
+  )
+}
+
+export default ChapionDetailPage 
---
+++ src/app/champions/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const ChampionsPage = () => {
+  return (
+    <div>ChampionsPage</div>
+  )
+}
+
+export default ChampionsPage
