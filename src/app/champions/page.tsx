14c15f35a22dd46ce34d8638560be0efd3979536
joycie416 <joycie416@gmail.com>
2024-10-03 16:41:17 +0900

    [update] change rendering method, [add] middleware.ts

--- src/app/champions/page.tsx
+++ src/app/champions/page.tsx
@@ -13,12 +13,22 @@ const ChampionsPage = async () => {
     <div className="w-full min-w-fit">
       <p className="text-2xl text-red-500 mb-4">Champions List</p>
       <div className="grid grid-cols-4 gap-4">
-        {data.map((info) => {
-          const [name, content] = info;
-          return (
-            <Card type={'champion'} name={name} title={content.name} text={content.title} key={name}/>
-          );
-        })}
+        {!data ? (
+          <>Loading...</>
+        ) : (
+          data.map((info) => {
+            const [name, content] = info;
+            return (
+              <Card
+                type={"champion"}
+                name={name}
+                title={content.name}
+                text={content.title}
+                key={name}
+              />
+            );
+          })
+        )}
       </div>
     </div>
   );
